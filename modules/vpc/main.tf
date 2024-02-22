3f06090e2048847374ed211a3ac0d29c73020d7d
lumberjoc <132397996+lumberjoc@users.noreply.github.com>
2024-02-21 20:12:30 -0800

    update: vpc and subnets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,7 +1,18 @@
 # VPC 
-
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cider
+  instance_tenancy = "default"
+}
 # Subnets
+resource "aws_subnet" "subnets" {
+  count = length(var.subnet_cidr)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.subnet_cidr[count.index]
 
+  tags = {
+    Name = "Main"
+  }
+}
 # Internet Gateway
 
 # Route Table
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -7,3 +7,9 @@ variable "subnet_cidr" {
   description = "Subnet CIDR's"
   type = list(string)
 }
+
+variable "subnet_names" {
+  description = "Subnet names"
+  type = list(string)
+  default = [ "public-subnet-1", "public-subnet-2" ]
+}
